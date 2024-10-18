7f2e1d020d5f8079d6d7c35b28047d6c24b3799c
23david14 <david18martinez23@gmail.com>
2024-10-18 16:21:45 -0500

    Archivo3 mejorado

--- archivo3.py
+++ archivo3.py
@@ -4,4 +4,17 @@ def suma(n1, n2):
     result = n1 + n2
     return result
 
+def division(n1, n2):
+    result = 0
+    try:
+        result = n1 / n2
+    except:
+        return "Error, número no divisible entre 0"
+
+    return result
+
+n1 = int(input("Ingrese un número: "))
+n2 = int(input("Ingrese un número: "))
+
 print(suma(2, 2))
+print(division(n1, n2))
